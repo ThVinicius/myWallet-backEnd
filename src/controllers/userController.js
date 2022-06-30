4c09df1ec40af96816a4c15282e155d6d2333208
Vinicius Pacheco <vinicioss9955@gmail.com>
2022-06-30 14:50:16 -0300

    POST /add completed

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,12 +1,13 @@
 import { db } from '../dbStrategy/mongo.js'
 import Joi from 'joi'
+import dayjs from 'dayjs'
+
+const tokenSchema = Joi.string()
+  .length(43)
+  .pattern(/^Bearer /)
+  .required()
 
 export const getWallet = async (req, res) => {
-  const tokenSchema = Joi.string()
-    .length(43)
-    .pattern(/^Bearer /)
-    .required()
-
   const { error } = tokenSchema.validate(req.headers.authorization)
 
   if (error) return res.sendStatus(400)
@@ -23,6 +24,42 @@ export const getWallet = async (req, res) => {
       .findOne({ _id: user.idUser })
 
     return res.status(200).send(operations)
+  } catch (error) {
+    console.log(error)
+    return res.status(500).send(error)
+  }
+}
+
+export const postAdd = async (req, res) => {
+  const bodySchema = Joi.object({
+    value: Joi.number().precision(2).greater(0).strict().required(),
+    description: Joi.string().trim().required(),
+    operation: Joi.string().valid('input', 'exit').required()
+  })
+
+  const { error } = bodySchema.validate(req.body)
+  const { error: tokenError } = tokenSchema.validate(req.headers.authorization)
+
+  if (error || tokenError) return res.sendStatus(400)
+
+  try {
+    const token = req.headers.authorization.replace('Bearer ', '')
+
+    const user = await db.collection('session').findOne({ token })
+    console.log(user)
+
+    if (user === null) return res.sendStatus(404)
+
+    const time = dayjs().format('DD/MM')
+
+    await db
+      .collection('users')
+      .findOneAndUpdate(
+        { _id: user.idUser },
+        { $push: { operations: { ...req.body, time } } }
+      )
+
+    return res.sendStatus(200)
   } catch (error) {
     console.log(error)
     return res.status(500).send(error)
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import express, { json } from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import { login, register } from './controllers/authController.js'
-import { getWallet } from './controllers/userController.js'
+import { getWallet, postAdd } from './controllers/userController.js'
 
 const app = express()
 
@@ -14,5 +14,6 @@ app.post('/login', login)
 app.post('/register', register)
 
 app.get('/wallet', getWallet)
+app.post('/add', postAdd)
 
 app.listen(5000)
